8efb98308010454b7943cef2a2a5960ad849701e
root <root@Sandbox.(none)>
2014-08-02 09:58:40 -0400

    cleared junk

--- prereqsInstall.sh
+++
@@ -1,6 +0,0 @@
-#Installs need modules for string analysis
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | sudo python
-git clone https://github.com/translate/translate.git
-cd translate
-python setup.py install
-
