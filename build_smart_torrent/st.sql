83c45a67a42607d146b76baad88ece2c878ae642
JaminB <jaminbecker@gmail.com>
2014-12-14 00:33:58 -0500

    Update st.sql

--- build_smart_torrent/st.sql
+++ build_smart_torrent/st.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS searches(id varchar(32), data LONGTEXT);
 CREATE DATABASE IF NOT EXISTS searches;
 USE searches;
 CREATE TABLE IF NOT EXISTS search_log(ID varchar(32), Term varchar(100), Category varchar(15), PRIMARY KEY (ID));
-#CREATE USER 'st'@'localhost' IDENTIFIED BY 'tech0nsite!';
-GRANT ALL ON  `database`.* TO 'st'@'localhost' IDENTIFIED BY 'tech0nsite!';
-GRANT ALL ON  `database`.* TO 'st'@'%' IDENTIFIED BY 'tech0nsite!';
-#CREATE USER 'st'@'%' IDENTIFIED BY 'tech0nsite!';
+#CREATE USER 'st'@'localhost' IDENTIFIED BY 'whateveryouwant';
+GRANT ALL ON  `database`.* TO 'st'@'localhost' IDENTIFIED BY 'whateveryouwant';
+GRANT ALL ON  `database`.* TO 'st'@'%' IDENTIFIED BY 'whateveryouwant';
+#CREATE USER 'st'@'%' IDENTIFIED BY 'whateveryouwant';
 #GRANT ALL PRIVILEGES ON *.* TO 'st'@'%';
 #GRANT ALL PRIVILEGES ON *.* TO 'st'@'localhost';
 
